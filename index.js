bc2fee3970e1ef8570e40e9b5cc387088395651b
Shahi77 <shahiinfinity02@gmail.com>
2024-03-28 18:55:47 +0530

    function to generate short urls

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 
+const urlRoute = require("./routes/url.routes");
 const app = express();
 const PORT = 8001;
+
+app.use("/url", urlRoute);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
