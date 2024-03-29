badcb85bdee1b14f4bf1b3bcb6031ee7ad39993a
Shahi77 <shahiinfinity02@gmail.com>
2024-03-29 14:38:23 +0530

    to view total number of clicks

--- src/routes/staticRouter.js
+++ src/routes/staticRouter.js
@@ -1,9 +1,17 @@
 const express = require("express");
-
+const URL = require("../models/url");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  return res.render("home");
+router.get("/", async (req, res) => {
+  try {
+    const allurls = await URL.find({});
+    return res.render("home", {
+      urls: allurls,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
